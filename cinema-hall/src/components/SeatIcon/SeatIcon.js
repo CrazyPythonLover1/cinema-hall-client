c4ccd14f17d6f3e207a399c66be89b61be2b56e5
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-12-20 15:24:48 -0800

    seatIcon

--- cinema-hall/src/components/SeatIcon/SeatIcon.js
+++ cinema-hall/src/components/SeatIcon/SeatIcon.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import "./SeatIcon.scss";
+import EventSeatOutlinedIcon from "@material-ui/icons/EventSeatOutlined";
+import { IconButton } from "@material-ui/core";
+import { UserContext } from "../../App";
+// import { useDispatch, useSelector } from "react-redux";
+// import { selectMovie, setMovieInfo } from "../../features/appSlice";
 
-const SeatIcon = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+function SeatIcon() {
+  const [booked, setBooked] = useState(false);
+
+//   const dispatch = useDispatch();
+//   const movie = useSelector(selectMovie);
+const [movieInfo, setMovieInfo] = useContext(UserContext)
+  const numBooked = movieInfo?.numberofBookings;
+  return (
+    <IconButton>
+      {" "}
+      <EventSeatOutlinedIcon
+        style={{ fill: booked ? `red` : `currentcolor` }}
+        onClick={() => {
+          console.log("clicked");
+          setBooked(true);
+
+        //   dispatch(
+        //     setMovieInfo({
+        //       ...movieInfo,
+        //       numberofBookings: numBooked + 1,
+        //     })
+        //   );
+        }}
+      ></EventSeatOutlinedIcon>
+    </IconButton>
+  );
+}
 
 export default SeatIcon;
